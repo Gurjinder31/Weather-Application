f73b224935063decb16ec68e723a0af51dd8ee78
Gurjinder Singh <gs@hub.com>
2020-09-17 21:46:20 -0300

    Updates

---
+++ precache-manifest.325fc00b85ebd81469f5bc1c599050b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cc5f0322a0e9c5a9db7eb97d99202b",
+    "url": "/Weather-Application/index.html"
+  },
+  {
+    "revision": "c54a2698ce0032bf3de4",
+    "url": "/Weather-Application/static/css/main.98a8f5a2.chunk.css"
+  },
+  {
+    "revision": "384219d8f5165d130b1d",
+    "url": "/Weather-Application/static/js/2.62e50013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Application/static/js/2.62e50013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54a2698ce0032bf3de4",
+    "url": "/Weather-Application/static/js/main.8c4e6358.chunk.js"
+  },
+  {
+    "revision": "c1aeaf0b8ba3f91e7836",
+    "url": "/Weather-Application/static/js/runtime-main.ee19ae0f.js"
+  },
+  {
+    "revision": "c6494ceab54c7bc415dd0dc6b5b9157f",
+    "url": "/Weather-Application/static/media/app.c6494cea.jpg"
+  },
+  {
+    "revision": "5f6d919416381fabbcff825690f4868e",
+    "url": "/Weather-Application/static/media/cold.5f6d9194.jpg"
+  },
+  {
+    "revision": "ca9fe0a65bbc1253ede943fc9422f228",
+    "url": "/Weather-Application/static/media/hot.ca9fe0a6.jpg"
+  }
+]);
